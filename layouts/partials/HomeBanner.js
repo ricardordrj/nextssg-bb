afe8fd4950d48c8ac251ab7b950d27426bbe8160
Ricardo <ricardordrj@gmail.com>
2024-10-13 21:58:44 -0300

    corrigindo

--- layouts/partials/HomeBanner.js
+++ layouts/partials/HomeBanner.js
@@ -1,47 +1,60 @@
-import NotFound from "@layouts/404";
+"use client"
 import { markdownify } from "@lib/utils/textConverter";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { getBanner } from "services";
 
-const HomeBanner = async () => {
-  try {
 
-    const { data } = await getBanner();
-    console.log('banner', data.banner[0].button);
 
-    return (
-      <section className="section pb-[50px]">
-        <div className="container">
-          <div className="row text-center">
-            <div className="mx-auto lg:col-10">
-              <h1 className="font-primary font-bold">{data.banner[0].title}</h1>
-              <p className="mt-4">{markdownify(data.banner[0].content)}</p>
+const HomeBanner = () => {
+  const [bannerInfo, setBannerInfo] = useState();
 
-              <Link
-                className="btn btn-primary mt-4"
-                href={data.banner[0].button.link}
+  const banner = () => {
+    console.log('bannerInfo', bannerInfo);
 
-              >
-                {data.banner[0].button.label}
-              </Link>
+    getBanner()
+      .then(({ data }) => {
 
-              <Image
-                className="mx-auto mt-12"
-                src={data.banner[0].image.url}
-                width={750}
-                height={390}
-                alt="banner image"
-                priority
-              />
-            </div>
+        setBannerInfo(data.banner);
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar banner:', error);
+      });
+  };
+
+  useEffect(() => {
+    banner();
+  }, []);
+  return bannerInfo && (
+    <section className="section pb-[50px]">
+      <div className="container">
+        <div className="row text-center">
+          <div className="mx-auto lg:col-10">
+            <h1 className="font-primary font-bold">{bannerInfo[0].title}</h1>
+            <p className="mt-4">{markdownify(bannerInfo[0].content)}</p>
+
+            <Link
+              className="btn btn-primary mt-4"
+              href={bannerInfo[0].button.link}
+
+            >
+              {bannerInfo[0].button.label}
+            </Link>
+
+            <Image
+              className="mx-auto mt-12"
+              src={bannerInfo[0].image.url}
+              width={750}
+              height={390}
+              alt="banner image"
+              priority
+            />
           </div>
         </div>
-      </section>
-    );
-  } catch (e) {
-    return (<div>Not Found</div>)
-  }
+      </div>
+    </section>
+  );
 };
 
 export default HomeBanner;
--- services/index.js
+++ services/index.js
@@ -4,6 +4,7 @@ const getBanner = async () => {
   try {
     const { data } = await api.get("/home?populate[banner][populate]=image&populate[banner][populate]=button")
 
+
     return data
   } catch (error) {
     console.log(error)
